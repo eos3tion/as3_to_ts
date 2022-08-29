3810bd61c9bd2f599a9b67f1fab03765d6f24c2d
chengfang <chengfang@tpulse-tech.com>
2022-08-29 20:13:10 +0800

    修复staticfun命名bug

--- SolveAST.ts
+++ SolveAST.ts
@@ -808,7 +808,7 @@ function checkScope(node: AstNode, clzCnt: ClassContext, noAddThis?: boolean, ri
                     const { staticFuns, enumData } = clz;
                     if (staticFuns[right]) {
                         let funName = getStaticFunName(right, v);
-                        checkImp(v, clzCnt.impDict, `${right}:${funName}`);
+                        checkImp(v, clzCnt.impDict, `${right} as ${funName}`);
                         return "";
                     } else if (clz.isEnum() && enumData[right]) {
                         let na = getEnumClassName(v);
@@ -886,6 +886,8 @@ function getMemberAccessExpressionNodeStr(node: AstNode, clzCnt: ClassContext) {
     let left = checkScope(leftNode, clzCnt, false, right);
     if (left) {
         left = left + ".";
+    } else {//export 函数的特殊处理
+        right = getStaticFunName(right, getNodeStr(leftNode, clzCnt));
     }
     return `${left}${right}`;
 }
